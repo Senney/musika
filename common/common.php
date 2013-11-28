94a92f1ea4b9a8e7075e4cd30d7919efa64387ff
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 15:01:17 -0700

    Added a navbar file to abstract out the navbar for generalization.

---
+++ common/common.php
@@ -0,0 +1,9 @@
+<?php
+function load_bootstrap_css() {
+echo '
+<link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="libs/bootstrap/bootstrap-theme.css" rel="stylesheet">
+';
+}
+
+?>
---
+++ common/navbar.php
@@ -0,0 +1,20 @@
+<div class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<div class = "navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse">
+				<span class="sr-only">Toggle Navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php">Musika</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="">Home</a></li>
+				<li><a href="#">Library</a></li>
+				<li><a href="#">Search</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
