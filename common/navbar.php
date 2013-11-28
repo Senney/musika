f7793fe01e9e8894b5bba4370918ff778d2783ae
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 15:35:05 -0700

    Updated links on navbar.

--- common/navbar.php
+++ common/navbar.php
@@ -6,14 +6,27 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
 			</button>
 			<a class="navbar-brand" href="index.php">Musika</a>
 		</div>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="">Home</a></li>
-				<li><a href="#">Library</a></li>
+				<li><a href="#">My Music</a></li>
+				<li><a href="#">Playlists</a></li>
+				<li><a href="#">Discover</a></li>
 				<li><a href="#">Search</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-default navbar-right">
+				<!-- Add login check here. -->
+				<!--
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Register</a></li>
+				-->
+				<li>
+					<a href="#">Senney</a>
+				</li>
 			</ul>
 		</div>
 	</div>
