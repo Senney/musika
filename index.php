41c01027a03ea70288efd248311235f1a94d726e
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 15:02:32 -0700

    Generalized some portions of the page.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once "./common/common.php";
 ?>
 
 <!DOCTYPE html>
@@ -7,30 +7,14 @@
     <head>
         <title>Musika - Music Library</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="libs/bootstrap/bootstrap-theme.css" rel="stylesheet">
+        <?php
+        load_bootstrap_css();
+        ?>
     </head>
     <body>
-        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class = "navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Musika</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Library</a></li>
-                        <li><a href="#">Search</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+		<?php
+		require_once "./common/navbar.php";
+		?>
         <div class="jumbotron">
 			<div class="container">
 				<h1>Musika</h1>
