3fd01a2029bbc539420ffc37fe8f669b357f8adf
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 15:14:07 -0700

    Fixed a missing resource error in css inclusion.

--- common/common.php
+++ common/common.php
@@ -2,8 +2,7 @@
 function load_bootstrap_css() {
 echo '
 <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="libs/bootstrap/bootstrap-theme.css" rel="stylesheet">
-';
+<link href="libs/bootstrap/css/bootstrap-theme.css" rel="stylesheet">';
 }
 
 ?>
