5d41d8d766075aab099557e8853da3d5782c5fd6
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 18:21:48 -0700

    Fixed a bug in saving the cached image.

--- common/common.php
+++ common/common.php
@@ -5,10 +5,10 @@ echo '
 <link href="libs/bootstrap/css/bootstrap-theme.css" rel="stylesheet">';
 }
 
-define("IMAGE_CACHE_FOLDER", "./cached_images/");
+define("IMAGE_CACHE_FOLDER", dirname(__FILE__) . "/../cached_images/");
 function cache_image($image_name, $image_url) {
 	$contents = file_get_contents($image_url);
-	$ret = file_put_contents(IMAGE_CACHE_FOLDER . $image_name);
+	$ret = file_put_contents(IMAGE_CACHE_FOLDER . $image_name, $contents);
 	return IMAGE_CACHE_FOLDER . $image_name;
 }
 
