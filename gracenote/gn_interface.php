e49a5f5877d82939854ddc15097d9a7c01e0faa5
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 17:25:20 -0700

    Work on Gracenote interface.

--- gracenote/gn_interface.php
+++ gracenote/gn_interface.php
@@ -3,12 +3,33 @@ include_once("./gracenote.php");
 include_once("../php-gracenote/Gracenote.class.php");
 
 function populate_cache_song($song_title, $album_name="", $artist_name="") {
-	$api = get_gracenote_api();
+	$search_type = Gracenote\WebAPI\GracenoteWebAPI::ALL_RESULTS;
+	if ($album_name != "" || $artist_name != "")
+		$search_type = Gracenote\WebAPI\GracenoteWebAPI::BEST_MATCH_ONLY;
 	
+	$api = get_gracenote_api();
+	$results = $api->searchTrack($artist_name, $album_name, $song_title);
+	echo "Result Array: " . sizeof($results);
+	
+	// Loop over all the albums returned and get all songs from those albums for caching.
+	foreach ($results as $res) {
+		$gnid = $res["album_gnid"];
+		populate_cache_album_gnid($gnid);
+	}
 }
 
-function populate_cache_artist($album_name, $artist_name="") {
+function populate_cache_album_gnid($album_gnid) {
+	$api = get_gracenote_api();
+	$results = $api->fetchAlbum($album_gnid);
 	
+	// Parse the album information into an album object.
+	// Parse the artist information into an artist object.
+		// Ensure that "Various Artists" is handled properly. 
+		// Essentially this means that we step through each track on
+		// the record and attribute each artist to the album.
+	// Parse the track information.
 }
+
+populate_cache_song("Funkytown");
 
 ?>
--- gracenote/gracenote.php
+++ gracenote/gracenote.php
@@ -1,20 +1,27 @@
 <?php
 include_once("../php-gracenote/Gracenote.class.php");
 
-$clientID  = "9595136-CDC6FD136B4193F856BA76A6F2773EFC"; // Put your Client ID here.
+$clientID  = "9595136"; // Put your Client ID here.
 $clientTag = "CDC6FD136B4193F856BA76A6F2773EFC"; // Put your Client Tag here.
 
 function get_gracenote_userid() {
-	$userID = ""; /** Later: Populate this from the database. **/
+	global $clientID;
+	global $clientTag;
+	
+	$userID = "264114340339182498-A9B8E0B66E20EE19B21EF883DDFEC9A3"; /** Later: Populate this from the database. **/
 	if (!isset($userID) || $userID == "") {
 		$api = new Gracenote\WebAPI\GracenoteWebAPI($clientID, $clientTag);
 		$userID = $api->register();
 		/** Store the Gracenote API User ID here. **/
 	}
+	echo $userID;
 	return $userID;
 }
 
 function get_gracenote_api() {
+	global $clientID;
+	global $clientTag;
+	
 	$api = new Gracenote\WebAPI\GracenoteWebAPI($clientID, $clientTag, get_gracenote_userid());
 	return $api;
 }
