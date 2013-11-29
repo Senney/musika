db37e18bd86033e1b983b2533973e8ec3bf85c34
Sean Heintz <sean.heintz@gmail.com>
2013-11-29 14:07:12 -0700

    Added more includes to common css icnlude.

--- common/common.php
+++ common/common.php
@@ -1,8 +1,10 @@
 <?php
 function load_bootstrap_css() {
 echo '
+<link rel="icon" type="image/png" href="favicon.png">
 <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="libs/bootstrap/css/bootstrap-theme.css" rel="stylesheet">
+<link href="css/musika-common.css" rel="stylesheet">
 <script src="libs/bootstrap/js/bootstrap.min.js"></script>
 ';
 }
@@ -10,6 +12,8 @@ echo '
 define("IMAGE_CACHE_FOLDER", dirname(__FILE__) . "/../cached_images/");
 function cache_image($image_name, $image_url) {
 	$target_loc = IMAGE_CACHE_FOLDER . $image_name;
+	// Create the image folder if it doesn't exist.
+	if (!file_exists(IMAGE_CACHE_FOLDER)) mkdir(IMAGE_CACHE_FOLDER);  
 	if (file_exists($target_loc)) return;
 	
 	$contents = file_get_contents($image_url);
