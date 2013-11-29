1501f1d0f2423deb9284a12cc715f79f2c67c062
Sean Heintz <sean.heintz@gmail.com>
2013-11-28 17:25:25 -0700

    Added cache_image

--- common/common.php
+++ common/common.php
@@ -5,4 +5,11 @@ echo '
 <link href="libs/bootstrap/css/bootstrap-theme.css" rel="stylesheet">';
 }
 
+define("IMAGE_CACHE_FOLDER", "./cached_images/");
+function cache_image($image_name, $image_url) {
+	$contents = file_get_contents($image_url);
+	$ret = file_put_contents(IMAGE_CACHE_FOLDER . $image_name);
+	return IMAGE_CACHE_FOLDER . $image_name;
+}
+
 ?>
