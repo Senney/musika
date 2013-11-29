ea1b22a7f1fbe32f177e9eac154a3bb823a1e23f
Sean Heintz <sean.heintz@gmail.com>
2013-11-29 14:07:29 -0700

    Work on library display and ui func

---
+++ library.php
@@ -0,0 +1,37 @@
+<?php
+require_once "./common/common.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Musika - Music Library</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
+        load_bootstrap_css();
+        ?>
+        <script src="js/library.js"></script>
+    </head>
+    <body>
+		<?php
+		require_once "./common/navbar.php";
+		?>
+		<div class="container">
+			<div class="row">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<h2>My Music</h2>
+						<div class="input-group">
+							<strong>View: </strong>
+							<div class="btn-group" id="view-group">
+								<button type="button" class="btn btn-default" selected>Song</button>
+								<button type="button" class="btn btn-default">Album</button>
+								<button type="button" class="btn btn-default">Artist</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
