62d8d90bc57aa2208edfe4bf8878ee9d34f96eb4
Sean Heintz <sean.heintz@gmail.com>
2013-12-02 14:45:42 -0700

    FIxed database config.

--- database/database.config.php
+++ database/database.config.php
@@ -1,9 +1,9 @@
 <?php
 
 class dbConfig {
-	public static $server = "localhost";
-	public static $username = "root";
-	public static $password = "5*Hotel";
+	public static $server = "www.senney.net";
+	public static $username = "musika";
+	public static $password = "musika";
 	public static $database = "musika";
 }
 
