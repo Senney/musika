6e416233f4dad9090e0479887c89fcb241ac8c09
Sean Heintz <sean.heintz@gmail.com>
2013-12-01 18:34:48 -0700

    Work on registration handler

--- handlers/registration_handler.php
+++ handlers/registration_handler.php
@@ -1,6 +1,29 @@
 <?php
 require_once "../database/database.php";
+require_once "../workers/user_worker.php";
 
+$username = $_POST["username"];
+$password = $_POST["password"];
+$password_confirm = $_POST["passwordconfirm"];
+$email = $_POST["email"];
 
+// Verify all of our variables are set.
+if (!isset($username) || !isset($password) || !isset($password_confirm)
+		|| !isset($email)) 
+{
+	// Handle errors here.
+	header("Location: ../register.php");
+	exit(1);
+}
+
+if ($password != $password_confirm) {
+	// Handle passwords not matching (this is really only if they bypass
+	// the javascript like weirdos.
+	header("Location: ../register.php");
+	exit(1);
+}
+
+$worker = new UserWorker();
+$worker->registerUser($username, $password, $email);
 
 ?>
