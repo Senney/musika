c5239996e6d4b8859d51baf620531f514a1e0a41
Sean Heintz <sean.heintz@gmail.com>
2013-12-01 18:07:13 -0700

    Password verification added to login handler.

--- handlers/login_handler.php
+++ handlers/login_handler.php
@@ -1,6 +1,15 @@
 <?php
 require_once "../database/database.php";
 
+$link = get_mysqli_link();
+$query = "SELECT password FROM users WHERE username=?";
+$params = array($_POST["username"]);
+$result = mysqli_prepared_query($link, $query, "s", $params);
 
+if (password_verify($_POST["password"], $result["password"])) {
+	// Successfully verified. Setup user session here.
+} else {
+	// Verification failed, redirect to login page.
+}
 
 ?>
