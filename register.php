870a079fb179a31f432dad4e1cbc2b3633348a86
Sean Heintz <sean.heintz@gmail.com>
2013-12-01 18:38:56 -0700

    Fixed the action url for registration

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ require_once "common/common.php"
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-4 well well-sm">
 					<legend>Register for Musika</legend>
-					<form action="handlers/register_handler.php" method="post" class="form" role="form">
+					<form action="handlers/registration_handler.php" method="post" class="form" role="form">
 						<input class="form-control" name="username" placeholder="Username" type="text" />
 						<input class="form-control" name="email" placeholder="Email Address" type="email" />
 						<input class="form-control" name="password" placeholder="Password" type="password" />
