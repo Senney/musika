71c05abd65bd749f176e192e84ba7fb92abe8a42
Sean Heintz <sean.heintz@gmail.com>
2013-12-02 16:21:07 -0700

    Added search to song worker.

--- workers/song_worker.php
+++ workers/song_worker.php
@@ -2,7 +2,14 @@
 require_once "../database/database.php";
 
 class SongWorker {
-	
+	public function findSongName($song_name) {
+		$link = get_mysqli_link();
+		$query = "SELECT * FROM song JOIN (albumsongs JOIN album) " .
+			"WHERE title = ?";
+		$result = mysqli_prepared_query($link, $query, "s",
+			array(song_name));
+		return $result;
+	}
 }
 
 ?>
