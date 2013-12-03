9ceab1b0753bcbea5f58f0794da7315957fd673b
Sean Heintz <sean.heintz@gmail.com>
2013-12-02 18:19:02 -0700

    Updated to worker.

--- workers/song_worker.php
+++ workers/song_worker.php
@@ -2,15 +2,15 @@
 require_once "../database/database.php";
 
 class SongWorker {
-	public function findSongName($song_name) {
+	public function findSongName($song_name, $limit=10) {
 		$link = get_mysqli_link();
 		
 		$query = "SELECT * FROM ((song JOIN ".
 			"albumsongs ON albumsongs.songId = song.SID) ".
 			"JOIN album ON albumsongs.albumId = album.albumID) ".
-			"WHERE title LIKE ?;";
-		$result = mysqli_prepared_query($link, $query, "s",
-			array($song_name));
+			"WHERE title LIKE ? LIMIT ?;";
+		$result = mysqli_prepared_query($link, $query, "sd",
+			array($song_name, $limit));
 		return $result;
 	}
 }
