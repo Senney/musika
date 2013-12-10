ec386112a17447dd6bf911e286f9ef2e366f2176
Sean Heintz <sean.heintz@gmail.com>
2013-12-10 14:59:08 -0500

    Added some functionality to song addition handler.

---
+++ handlers/add_song_handler.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../common/common.php";
+
+if (!keys_exist($_POST, array(
+		"song-name", "artist-name", "album-name", "media-type"
+	))) {
+	header("Location: ../add.php?error=0");
+	exit(1);
+}
+
+$song_name = $_POST["song-name"];
+$artist_name = $_POST["artist-name"];
+$album_name = $_POST["album-name"];
+$media_type = $_POST["media-type"];
+
+	
+	
+?>
