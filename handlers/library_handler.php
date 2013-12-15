41daa182927442736a62c8c4019f5b2444980d10
Sean Heintz <sean.heintz@gmail.com>
2013-12-15 17:21:28 -0500

    Added library data handler.

---
+++ handlers/library_handler.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../common/common.php";
+require_once "../workers/ownership_worker.php";
+
+logged_in_redirect();
+
+$type = $_GET["type"];
+$userid = usrid();
+
+$worker = new OwnershipWorker($userid);
+if ($type == "song") {
+	$returndata = array("head" => array("Title", "Artist", "Album"), "data" => array());
+	$songs = $worker->getUserSongData();
+	$returndata["data"] = $songs;
+	print_r($returndata);
+}
+
+?>
