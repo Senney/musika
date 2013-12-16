2650bedc8be5cddd5fe36baeb8dd8f3af48ed2d7
Sean Heintz <sean.heintz@gmail.com>
2013-12-15 22:28:42 -0500

    Friend handler and worker completed.

--- handlers/friend_handler.php
+++ handlers/friend_handler.php
@@ -3,9 +3,9 @@ require_once "../database/database.php";
 require_once "../workers/friend_worker.php";
 require_once "../workers/user_worker.php";
 
-
+// www.senney.net/musika/handlers/friend_handler.php?userid=1&type=2
 if (!keys_exist($_GET, array(
-		"userid"
+		"userid","type"
 	))) {
 	header("Location: ../index.php?error=0");
 	exit(1);
@@ -14,7 +14,12 @@ if (!keys_exist($_GET, array(
 
 $friend_worker = new FriendWorker();
 $user_friend = $_GET["userid"];
-
+$type = $_GET["type"];
+if($type = 1){
 $friend_worker->addFriend($user_friend);
+}
+elseif($type = 2){
+$friend_worker->deleteFriend($user_friend);
+}
 
 ?>
--- workers/friend_worker.php
+++ workers/friend_worker.php
@@ -8,6 +8,27 @@ class FriendWorker{
 			$result = mysqli_prepared_query($link,$query,"dd",array(usrid(),$userid);
 		
 		}
-
+		public function getUserFriend($userid){
+			$link = get_mysqli_link();
+			$query = "SELECT * FROM friend JOIN user ON friend.FriendId  == users.UserId ".
+			"WHERE friend.FriendId = ?"
+			
+			$result = mysqli_prepared_query($link, $query, "d",array($userid)); 
+			
+		
+			if(empty($result)) return false;
+			return $result[0];
+		}
+		
+		public function deleteFriend($userid)
+		{
+		$link = get_mysqli_link();
+		$query = "DELTE FROM friend WHERE friend.UserId = ? AND friend.FriendId = ?"
+		$result = mysqli_prepared_query($link,$query,"dd",array(usrid(),$userid));
+		
+		
+		
+		}
+		
 
 }
