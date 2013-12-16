bacbd82a9fcb58b4a0f8b3ef6b08c14fffb621e1
Sean Heintz <sean.heintz@gmail.com>
2013-12-15 20:11:30 -0500

    Updated to handlers. Friend handler included.

--- handlers/friend_handler.php
+++ handlers/friend_handler.php
@@ -1,12 +1,20 @@
 <?php
 require_once "../database/database.php";
-require_once "../workers/friend_worker.php;
+require_once "../workers/friend_worker.php";
 require_once "../workers/user_worker.php";
 
 
+if (!keys_exist($_GET, array(
+		"userid"
+	))) {
+	header("Location: ../index.php?error=0");
+	exit(1);
+}
 
 
+$friend_worker = new FriendWorker();
+$user_friend = $_GET["userid"];
 
-
+$friend_worker->addFriend($user_friend);
 
 ?>
