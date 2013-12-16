e2c222c4bdda4bcd350ac69ed3eeb213c7f2ea73
Sean Heintz <sean.heintz@gmail.com>
2013-12-15 22:30:05 -0500

    Playlist worker complete

--- workers/playlist_worker.php
+++ workers/playlist_worker.php
@@ -10,15 +10,28 @@ class PlaylistWorker{
 
 	public function getPlaylist($pId){
 		$link = get_mysqli_link();
-		$query = "SELECT * FROM playlist WHERE playlist.pId = ?"
+		$query = "SELECT * FROM playlist WHERE playlist.pId = ?";
 		$result = mysqli_prepared_query($link, $query, "d", array($pId));
 		return = $result;
 	}
 
 	public function getPlaylistSongs($pId){
-	
-	
+		$link = get_mysqli_link();
+		$query = "SELECT * FROM playlistentry WHERE playlistentry.pId = ?";
+		$result = mysqli_prepared_query($link, $query, "d", array($pId));
+		return = $result;
 	}
-
+	
+	public function sharePlaylist($userId, $pId, $message = null){
+		$link = get_mysqli_link();
+		$query = "INSERT INTO playlistshare Values(?, ?, ?)";
+		$result = mysqli_prepared_query($link, $query, "dds", array($userId, $pId, $message));
+	}
+	
+	public function addRating($userId, $pId, $rating){
+		$link = get_mysqli_link();
+		$query = "INSERT INTO playlistrating Values(?, ?, ?)";
+		$result = mysqli_prepared_query($link, $query, "ddd", array($userId, $pId, $rating));
+	}
 }
 ?>
