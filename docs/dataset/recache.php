b5709ba92ed9024851bb84d3bc2e64cf721d8e65
Sean Heintz <sean.heintz@gmail.com>
2013-12-19 01:07:10 -0500

    Added a recache php script.

---
+++ docs/dataset/recache.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "../../database/database.php";
+require_once "../../workers/cache_worker.php";
+require_once "../../workers/artist_worker.php";
+
+$link = get_mysqli_link();
+$query = "SELECT artistId FROM itunescache";
+$result = mysqli_prepared_query($link, $query);
+
+$cw = new CacheWorker();
+$aw = new ArtistWorker();
+foreach ($result as $r) {
+	$name = $aw->getArtist($r["artistId"])["name"];
+	printf("Recaching: %s\n", $name);
+	$ret = $cw->cache_artist($name);
+	printf("Recached: %s -- %d\n", $name, $ret);
+	sleep(1);
+}
+
+?>
